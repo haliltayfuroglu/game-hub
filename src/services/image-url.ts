f7e0a563a80c99411075b21d5f03ee565b6c65b1
Halil Tayfuroglu <halil.tayfuroglu@hotmail.com>
2024-09-30 12:57:09 -0400

    Get optimized images..

---
+++ src/services/image-url.ts
@@ -0,0 +1,17 @@
+const getCroppedImageUrl = (url: string, width:number=600, length:number=400) => {
+
+    const target = 'media/';
+    const index = url.indexOf(target) + target.length;
+
+    const newUrl = url.slice(0, index)+ `crop/${width}/${length}/`+ url.slice(index);
+
+
+    console.log(newUrl);;
+
+    return newUrl;
+    
+
+}
+
+
+export default getCroppedImageUrl;
