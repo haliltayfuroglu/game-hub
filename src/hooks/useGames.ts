e4cab94e39a1f84edb843403f87a0041e532e5dc
Halil Tayfuroglu <halil.tayfuroglu@hotmail.com>
2024-09-30 13:11:43 -0400

    Show loading skeletons..

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -49,21 +49,30 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
   
     useEffect(() => {
         const controller = new AbortController();
+        setIsLoading(true);
+
+         new Promise(resolve => setTimeout(resolve, 1000));
+
         apiClient
             .get<FetchGamesResponse>("/games", {signal:controller.signal})
-            .then((res) => setGames(res.data.results))
+            .then((res) => {
+              setGames(res.data.results);
+              setIsLoading(false);
+            })
             .catch((err) => {
             if(err instanceof CanceledError) {return;}
             console.error(err);
             setError(err.message);
+            setIsLoading(false);
         });
+        
         return () => controller.abort();
     }, []);
-
-    return { games, error };
+    return { games, error, isLoading };
 }
 
 
